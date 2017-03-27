95e5d3a55a9ebac901c51163c7e3df06e68a4f97
corinnecm <corinne.carlson5@gmail.com>
2017-03-27 14:20:51 -0700

    Modified datetime func

--- src/cleanup.py
+++ src/cleanup.py
@@ -42,3 +42,8 @@ def get_sale_mon_yr(data):
     data['sale_date'] = pd.to_datetime(data['saledate'])
     data['sale_year'] = pd.DatetimeIndex(data['sale_date']).year
     data['sale_month'] = pd.DatetimeIndex(data['sale_date']).month
+    data.drop('saledate', axis=1)
+
+get_saledate_mon_yr(df)
+
+df.head()
