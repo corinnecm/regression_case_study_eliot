eccabb1d947234bdbc4cd90e8f232acc2ec19ed0
Eliot Logan <eliot.logan@gmail.com>
2017-03-27 14:02:39 -0700

    addded year cleanup

--- src/cleanup.py
+++ src/cleanup.py
@@ -8,4 +8,9 @@ df = pd.read_csv('data/Train.csv')
 year = df['YearMade']
 year = year[year != 1000]
 
+def clean_year_made(data):
+    data = data[(data['YearMade'] > 1920) & (data['YearMade'] <= 2017)]
+    return data
+
+
 price_v_year = df[['SalePrice', 'YearMade']]
